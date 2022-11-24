d445f4a60b2c7c35c6bb58e6d01ae25a0f987a55
s.bhavsar <s.bhavsar@wmt.team>
2022-11-24 17:09:23 +0100

    Porject files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![no_main]
 #![no_std]
 
-use stm32g0xx_hal::prelude::*;
+use cortex_m_rt::entry;
 use panic_probe as _;
 use rtt_target::{rprintln, rtt_init_print};
+use stm32g0xx_hal::prelude::*;
 use stm32g0xx_hal::stm32;
-use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
@@ -18,25 +18,25 @@ fn main() -> ! {
     let mut rcc = dp.RCC.constrain();
 
     let gpioa = dp.GPIOA.split(&mut rcc);
-   
+
     let mut pin_led_green = gpioa.pa3.into_push_pull_output();
     let mut pin_switch = gpioa.pa0.into_push_pull_output();
     let mut pin_led_red_128 = gpioa.pc6.into_push_pull_output();
     let mut pin_led_red_64 = gpioa.pa8.into_push_pull_output();
-    let mut pin_led_red_32 =  gpioa.pb1.into_push_pull_output();
+    let mut pin_led_red_32 = gpioa.pb1.into_push_pull_output();
     let mut pin_led_red_16 = gpioa.pb0.into_push_pull_output();
-    let mut pin_led_red_8 =  gpioa.pa7.into_push_pull_output();
-    let mut pin_led_red_4 =  gpioa.pa6.into_push_pull_output();
-    let mut pin_led_red_2 =  gpioa.pa5.into_push_pull_output();
-    let mut pin_led_red_1 =  gpioa.pa4.into_push_pull_output();
+    let mut pin_led_red_8 = gpioa.pa7.into_push_pull_output();
+    let mut pin_led_red_4 = gpioa.pa6.into_push_pull_output();
+    let mut pin_led_red_2 = gpioa.pa5.into_push_pull_output();
+    let mut pin_led_red_1 = gpioa.pa4.into_push_pull_output();
 
     pin_led_green.set_low().ok();
 
     loop {
-        for _ in 0..1_000_000{
+        for _ in 0..1_000_000 {
             pin_led_green.set_low().ok();
         }
-        for _ in 0..1_000_000{
+        for _ in 0..1_000_000 {
             pin_led_green.set_high().ok();
         }
     }
